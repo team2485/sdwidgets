9861b02a0785d251d9ec5d73c378f861996d58e7
Bryce <bmats@users.noreply.github.com>
2014-02-26 17:49:56 -0800

    Adding G13 keypress sender.

--- src/team2485/smartdashboard/extension/G13Sender.java
+++ src/team2485/smartdashboard/extension/G13Sender.java
@@ -3,112 +3,152 @@ package team2485.smartdashboard.extension;
 import edu.wpi.first.smartdashboard.gui.StaticWidget;
 import edu.wpi.first.smartdashboard.properties.Property;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
-import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
-import net.java.games.input.*;
+import javax.swing.JOptionPane;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
 
 /**
  * Guitar Hero X-plorer strummer axis sender
  * @author Bryce Matsumori
  */
-public class StrummerSender extends StaticWidget implements Runnable {
-    public static final String NAME = "X-plorer Strummer Sender";
-
-    private ControllerEnvironment env;
-    private Controller xplorer;
-    private Component component;
+public class G13Sender extends StaticWidget {
+    public static final String NAME = "G13 Sender";
+    
     private NetworkTable table;
-    private BufferedImage image;
+    private BufferedImage image, imageActivity;
+    
+    private boolean activity;
+    private final HashMap<Character, Integer> keystrokeMap;
+    private final ScheduledExecutorService activityExec = Executors.newSingleThreadScheduledExecutor();
+    private final Runnable hideActivity = new Runnable() {
+        @Override
+        public void run() {
+            activity = false;
+            repaint();
+        }
+    };
+    private ScheduledFuture activityFuture;
 
-    public StrummerSender() {
-        final Dimension size = new Dimension(84, 38);
+    public G13Sender() {
+        final Dimension size = new Dimension(54, 81);
         setSize(size);
         setPreferredSize(size);
         setMinimumSize(size);
         setMaximumSize(size);
 
         try {
-            image = ImageIO.read(getClass().getResource("/team2485/smartdashboard/extension/res/guitar.png"));
+            image = ImageIO.read(getClass().getResource("/team2485/smartdashboard/extension/res/G13.png"));
+            imageActivity = ImageIO.read(getClass().getResource("/team2485/smartdashboard/extension/res/G13-activity.png"));
         } catch (IOException e) { }
-    }
-
-    @Override
-    protected void paintComponent(Graphics gg) {
-        Graphics2D g = (Graphics2D)gg;
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        g.drawImage(image, 0, 0, null);
-
-        if (component == null) {
-            g.setColor(Color.red);
-            g.setStroke(new BasicStroke(3, BasicStroke.CAP_ROUND, BasicStroke.CAP_BUTT));
-            g.drawLine(10, 10, 74, 28);
-            g.drawLine(10, 28, 74, 10);
-        }
+        
+        // <editor-fold defaultstate="collapsed" desc="Keystroke -> G key map">
+        keystrokeMap = new HashMap<>(24);
+        keystrokeMap.put('Q', 1);
+        keystrokeMap.put('W', 2);
+        keystrokeMap.put('E', 3);
+        keystrokeMap.put('R', 4);
+        keystrokeMap.put('T', 5);
+        keystrokeMap.put('Y', 6);
+        keystrokeMap.put('U', 7);
+        
+        keystrokeMap.put('A', 8);
+        keystrokeMap.put('S', 9);
+        keystrokeMap.put('D', 10);
+        keystrokeMap.put('F', 11);
+        keystrokeMap.put('G', 12);
+        keystrokeMap.put('H', 13);
+        keystrokeMap.put('J', 14);
+        
+        keystrokeMap.put('Z', 15);
+        keystrokeMap.put('X', 16);
+        keystrokeMap.put('C', 17);
+        keystrokeMap.put('V', 18);
+        keystrokeMap.put('B', 19);
+        
+        keystrokeMap.put('N', 20);
+        keystrokeMap.put('M', 21);
+        keystrokeMap.put(',', 22);
+        
+        keystrokeMap.put('L', 23);
+        keystrokeMap.put('.', 24);
+        // </editor-fold>
     }
 
     @Override
     public void init() {
-        table = NetworkTable.getTable("SmartDashboard");
+        table = NetworkTable.getTable("G13");
 
-        env = ControllerEnvironment.getDefaultEnvironment();
-        for (Controller c : env.getControllers()) {
-            if (c.getName().contains("X-plorer")) {
-                System.out.println("Found X-plorer");
-                xplorer = c;
-                component = xplorer.getComponent(Component.Identifier.Axis.POV);
-
-                Thread t = new Thread(this, "Strummer Sender");
-                t.setDaemon(true);
-                t.start();
-            }
-        }
-
-        if (xplorer == null) {
-            final Runnable self = this;
-            env.addControllerListener(new ControllerListener() {
-                @Override
-                public void controllerAdded(ControllerEvent ce) {
-                    if (ce.getController().getName().contains("X-plorer")) {
-                        System.out.println("Found X-plorer");
-                        xplorer = ce.getController();
-                        component = xplorer.getComponent(Component.Identifier.Axis.POV);
-
-                        final Thread t = new Thread(self, "Strummer Sender");
-                        t.setDaemon(true);
-                        t.start();
+        GlobalScreen.getInstance().addNativeKeyListener(new NativeKeyListener() {
+            @Override
+            public void nativeKeyPressed(NativeKeyEvent nke) {
+                if (nke.getModifiers() == 11) { // ctrl + shift + alt
+                    switch (nke.getKeyCode()) {
+                        case NativeKeyEvent.VK_CONTROL:
+                        case NativeKeyEvent.VK_SHIFT:
+                        case NativeKeyEvent.VK_ALT:
+                            break;
+                        default: {
+                            try {
+                                final int key = keystrokeMap.get((char)nke.getKeyCode());
+                                table.putBoolean(Integer.toString(key), true);
+                                
+                                activity = true;
+                                repaint();
+                                if (activityFuture != null) activityFuture.cancel(false);
+                                activityFuture = activityExec.schedule(hideActivity, 500, TimeUnit.MILLISECONDS);
+                            }
+                            catch (NullPointerException ex) {
+                                System.err.println("Could not map key '" + (char)nke.getKeyCode() + "' (" + nke.getKeyCode() + ").");
+                            }
+                        }
                     }
                 }
+            }
 
-                @Override
-                public void controllerRemoved(ControllerEvent ce) {
-                }
-            });
+            @Override
+            public void nativeKeyReleased(NativeKeyEvent nke) {
+            }
+
+            @Override
+            public void nativeKeyTyped(NativeKeyEvent nke) {
+            }
+        });
+        
+        try {
+            GlobalScreen.registerNativeHook();
         }
+        catch (NativeHookException ex) {
+            System.err.println("Could not register native key hook!");
+            ex.printStackTrace();
+            
+            String confirm;
+            do {
+                confirm = JOptionPane.showInputDialog(null, "WARNING: Could not register native key hook. This means the G13 is UNUSABLE!\nEnter \"OK\" to dismiss.\n\n" + ex.getMessage(), "G13 Sender", JOptionPane.ERROR_MESSAGE);
+            } while (!confirm.equalsIgnoreCase("ok"));
+        }
+    }
+    
+    @Override
+    protected void paintComponent(Graphics gg) {
+        Graphics2D g = (Graphics2D)gg;
+
+        g.drawImage(activity ? imageActivity : image, 0, 0, null);
     }
 
     @Override
     public void propertyChanged(Property property) {
-    }
-
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                if (xplorer != null) {
-                    xplorer.poll();
-                    table.putNumber("xplorer-strummer", component.getPollData());
-                }
-
-                Thread.sleep(20);
-            }
-        } catch (InterruptedException e) { }
     }
 }
